b1e499437f03979142f5e3739bcce31e0bc075df
jferris72 <jferris@ualberta.ca>
2016-09-27 17:42:09 -0600

    Implement and test add, has, get tweet.

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,42 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by jferris on 27/09/16.
+ */
+public class TweetListTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+    public TweetListTest() {
+        super(LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet() {
+        TweetList list = new TweetList();
+
+        Tweet tweet = new NormalTweet("Hello!");
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testHasTweet() {
+        TweetList list = new TweetList();
+
+        Tweet tweet = new NormalTweet("Hello!");
+        assertFalse(list.hasTweet(tweet));
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweet() {
+        TweetList list = new TweetList();
+
+        Tweet a = new NormalTweet("Hello!");
+        Tweet b = new NormalTweet("HI!");
+
+        list.add(a);
+        list.add(b);
+
+        assertEquals(a, list.getTweet(0));
+        assertEquals(b, list.getTweet(1));
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jferris on 27/09/16.
+ */
+public class TweetList {
+    List<Tweet> tweets = new ArrayList<Tweet>();
+    public void add(Tweet tweet) {
+        tweets.add(tweet);
+    }
+
+    public boolean hasTweet(Tweet tweet) {
+        return tweets.contains(tweet);
+    }
+
+    public Tweet getTweet(int i) {
+        return tweets.get(i);
+    }
+}
